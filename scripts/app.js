222d1a2726695f7d575a38b4e553d04959ddd62f
BanuelosMendezJordi <jordi.banuelos17@tectijuana.edu.mx>
2022-06-20 17:11:43 -0700

    Task status (assignment 4)

--- scripts/app.js
+++ scripts/app.js
@@ -33,14 +33,73 @@ function saveTask() {
     isImportant
   );
   console.log(myTask);
-  displayTask(myTask);
+
+  $.ajax({
+    url: "https://fsdiapi.azurewebsites.net/api/tasks/",
+    type: "POST",
+    data: JSON.stringify(myTask),
+    contentType: "application/json",
+    success: function (response) {
+      let savedTask = JSON.parse(response); //Parse JSON response into this object
+      displayTask(savedTask);
+    },
+    error: function (details) {
+      console.log("Error saving:", details);
+    },
+  });
+}
+
+/**
+ * send a get request to https://fsdiapi.azurewebsites.net/api/tasks/
+ * response => json string
+ * parse the response => array
+ * console log the array
+ */
+function fetchTasks() {
+  $.ajax({
+    url: "https://fsdiapi.azurewebsites.net/api/tasks",
+    type: "GET",
+    success: function (response) {
+      let tasks = JSON.parse(response); // Array of tasks
+      // for loop, get every object and send it to displayTask();
+      // Before you display the tasks, check if it's yours, and only then display it
+      for (let i = 0; i < tasks.length; i = i + 1) {
+        let item = tasks[i];
+        if (item.name == "Jordi") {
+          displayTask(item);
+        }
+      }
+    },
+    error: function (dets) {
+      console.log("Error fetching tasks: ", dets);
+    },
+  });
+}
+
+function getStatusText(taskStatus) {
+  switch (taskStatus) {
+    case "0":
+      return "New";
+    case "1":
+      return "In Progress";
+    case "2":
+      return "Blocked";
+    case "3":
+      return "Completed";
+    case "4":
+      return "Removed";
+    default:
+      return "Missing";
+  }
 }
 
 function displayTask(myTask) {
+  let statusText = getStatusText(myTask.taskStatus);
   let syntax = `
   <div class="task">
     <h4>${myTask.taskTitle}</h4>
     <label>${myTask.taskLocation}</label>
+    <label>${statusText}</label>
     <div class="dates">
         <label>${myTask.taskDuration} days</label>
         <label>${myTask.taskDeadline}</label>
@@ -48,6 +107,19 @@ function displayTask(myTask) {
   </div>`;
 
   $("#task-list").append(syntax);
+}
+
+function testRequest() {
+  $.ajax({
+    url: "https://fsdiapi.azurewebsites.net/",
+    type: "GET",
+    success: function (response) {
+      console.log(response);
+    },
+    error: function (errorDetails) {
+      console.log("Error on request", errorDetails);
+    },
+  });
 }
 
 function tooglePanel() {
@@ -61,14 +133,32 @@ function tooglePanel() {
   }
 }
 
+// Delete all your tasks
+// DELETE request to https://fsdiapi.azurewebsites.net/api/tasks/clear/YOURNAME
+function clearAllTasks() {
+  $.ajax({
+    type: "DELETE",
+    url: "https://fsdiapi.azurewebsites.net/api/tasks/clear/Jordi",
+    success: function () {
+      $("#task-list").html("");
+    },
+    error: function (err) {
+      console.error(err);
+    },
+  });
+}
+
 function init() {
   // runTests();
   console.log("Task Manager");
   //Load data
+  fetchTasks();
 
   // Hook events
   $("#icon-important").click(toogleImportant);
   $("#btnShowHide").click(tooglePanel);
+  $("#btnSave").click(saveTask);
+  $("#btnDeleteTasks").click(clearAllTasks);
 }
 
 window.onload = init;
--- scripts/task.js
+++ scripts/task.js
@@ -1,13 +1,21 @@
 class Task {
-    constructor (id, taskTitle, taskDuration, taskDeadline, taskLocation, taskStatus, important){
-        this.id = id;
-        this.taskTitle = taskTitle;
-        this.taskDuration = taskDuration;
-        this.taskDeadline = taskDeadline;
-        this.taskLocation = taskLocation;
-        this.taskStatus = taskStatus;
-        this.important = important;
+  constructor(
+    id,
+    taskTitle,
+    taskDuration,
+    taskDeadline,
+    taskLocation,
+    taskStatus,
+    important
+  ) {
+    this.id = id;
+    this.taskTitle = taskTitle;
+    this.taskDuration = taskDuration;
+    this.taskDeadline = taskDeadline;
+    this.taskLocation = taskLocation;
+    this.taskStatus = taskStatus;
+    this.important = important;
 
-        this.name="Jordi";
-    }
+    this.name = "Jordi";
+  }
 }
